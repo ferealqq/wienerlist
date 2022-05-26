5890da34b207285fa8cb0a870f36c2d6a275836d
ferealqq <pekka.mattinen@webso.fi>
2022-05-26 23:32:11 +0300

    Tailwind & wasmserve config included

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  content: ["./**/*.{html,go}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./**/*.{html,go}"],
+	theme: {
+	  extend: {},
+	},
+	plugins: [],
 }
